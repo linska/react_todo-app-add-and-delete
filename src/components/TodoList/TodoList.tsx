72f18150808572948bd4a0459e4bd9651c1e81a5
linska <linskatanya@gmail.com>
2026-05-19 20:23:29 +0300

    feat: implement add and delete functions

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
 import { TodoItem } from '../TodoItem';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 interface Props {
   todoList?: Todo[];
@@ -18,22 +17,15 @@ export const TodoList: React.FC<Props> = ({
 }) => {
   return (
     <section className="todoapp__main" data-cy="TodoList">
-      <TransitionGroup>
-        {todoList.map(todo => (
-          <CSSTransition key={todo.id} timeout={300} classNames="item">
-            <TodoItem
-              todo={todo}
-              onDelete={onDeleteTodo}
-              loading={isDeletingCompletedTodos && todo.completed}
-            />
-          </CSSTransition>
-        ))}
-        {tempTodo && (
-          <CSSTransition key="temp-todo" timeout={300} classNames="temp-item">
-            <TodoItem todo={tempTodo} loading={true} />
-          </CSSTransition>
-        )}
-      </TransitionGroup>
+      {todoList.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          onDelete={onDeleteTodo}
+          loading={isDeletingCompletedTodos && todo.completed}
+        />
+      ))}
+      {tempTodo && <TodoItem todo={tempTodo} loading={true} />}
     </section>
   );
 };
